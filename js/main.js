57aa2c09f8471ef831641d645d0e6ff5ee06233d
Евгений Киенко <gr943_kes@lab-402-10.tomtit-tomsk.ru>
2026-02-16 11:15:53 +0700

    Выполнено практическое задание №2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-let app = new Vue ({
+let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks."
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
     }
 })
