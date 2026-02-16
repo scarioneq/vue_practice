041dd99dfcfced174737256ca2dcc747705bd555
Евгений Киенко <gr943_kes@lab-402-10.tomtit-tomsk.ru>
2026-02-16 12:14:36 +0700

    Выполнена практическая работа №5 (написание метода удаления из корзины)

--- js/main.js
+++ js/main.js
@@ -2,21 +2,40 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        description: "A pair of warm, fuzzy socks.",
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
+        inventory: 100,
+        onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
-    }
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        deleteFromCart() {
+            this.cart -= 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
+        }
+    },
+
 })
 
